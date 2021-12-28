42bed64944cd9d9c7fcfc0011ff2f0ff932b802f
Вадим Цимбалюк <vadymonua@gmail.com>
2021-12-28 12:04:55 +0200

    masonry layout, favicon, scrollTo, animate for scroliing

--- app/js/script.js
+++ app/js/script.js
@@ -1,28 +1,51 @@
-$('.comments').masonry({
-    // options...
-    itemSelector: '.item',
-    columnWidth: '.item',
-    percentPosition: true,
-    gutter: 12
-});
+$(document).ready(function (){
 
-//
- $('header nav .nav-item .nam-link').click(function (){
-     const body = $("html, body");
-     const scrollTo = $($(this).attr('href')).offset().top;
-     body.stop().animate({scrollTop: scrollTo}, 500, 'swing', function() {
-         console.log(this)
-     });
- });
+    const hash = window.location.hash;
 
-$(window).scroll(function(){
-    const scroll = $(window).scrollTop();
+    const body = $("html, body");
     const header = $('header');
-    const headerHeight = header.outerHeight();
+    const headerHeigth = header.outerHeight();
 
-    if (scroll >= headerHeight)
-        header.addClass('.shadow');
-    else
-        header.removeClass('.shadow');
-});
+    scrollToElement(hash);
+
+    function scrollToElement(selectorToScrollElement, callback = null){
+            if(!selectorToScrollElement) return;
+
+            const scrollTo = (($(selectorToScrollElement)).offset().top) - headerHeigth;
+
+            $('header nav .nav-item.active').removeClass('active');
+            body.stop().animate({scrollTop: scrollTo}, 500, 'swing', ()=> {
+                $(`a.nav-link[href="${selectorToScrollElement}"]`).parent().addClass('active');
+                if(callback) callback()
+            });
+    };
+
+    $('.comments').masonry({
+        // options...
+        itemSelector: '.item',
+        columnWidth: '.item',
+        percentPosition: true,
+        gutter: 12
+    });
+
+    $('header nav .nav-item .nav-link').click(function (){
+        scrollToElement($(this).attr('href'));
+    });
+
+    $('a.navbar-brand').click(function (){
+        scrollToElement($(this).attr('href'), ()=>{
+            window.location.hash = '';
+        });
+    })
+
+    $(window).scroll(function(){
+        const scrollTo = $(window).scrollTop();
+
+        if (scrollTo >= headerHeigth)
+            header.addClass('shadow');
+        else
+            header.addClass('shadow');
+    });
+
+})
 
